efcbe1caa24bef4304b9c239926b86b0f6a844e3
chobe111 <chobe1@naver.com>
2022-08-20 23:31:36 +0900

    Update recommendation logic add signupfavorite

--- services/recommendation_service.py
+++ services/recommendation_service.py
@@ -8,8 +8,6 @@ from typing import Dict, List
 class RecommendationService: 
     @inject
     def __init__(self, db: MongoClient):
-        # 
-        print("reco start")
         self.db = db['chordplay']
     
     def test(self):
@@ -25,53 +23,58 @@ class RecommendationService:
         tags = list(map(lambda x: x['tags'] if 'tags' in x.keys() else '',video_collection.find({'_id': video_id})))
         if len(tags) == 0:
             return ['']
-        
         tags = tags[0]
         lower_tags = list(map(lambda x: x.lower(), tags))
-        
         return lower_tags
-
     
-    def get_user_tag_map(self, user_id: str):
-        user_tag_map = {}
-    #   sort by date
+    def update_tag_map(self, user_tag_map, video_id, addCount):
+        tags = self.get_lower_tags(video_id)
+        for tag in tags:
+            if tag not in user_tag_map.keys():
+                user_tag_map[tag] = addCount
+                continue
+                
+            user_tag_map[tag] += addCount
+
+    def create_watch_history_user_tag_map(self, user_tag_map, user_id: str):
         watch_history_collection = self.db['WATCH_HISTORY']
         watch_info_list = watch_history_collection.find({'user': DBRef('USER', ObjectId(f'{user_id}'))}).sort("last_played", -1)
         
         for watch_info in watch_info_list[:min(40, len(watch_info_list))]:
             video_id = watch_info['video'].id
             count = watch_info['play_count']
-            tags = self.get_lower_tags(video_id)
-            for tag in tags:
-                if tag not in user_tag_map.keys():
-                    user_tag_map[tag] = count
-                    continue
-                    
-                user_tag_map[tag] += count
-                    
+            self.update_tag_map(user_tag_map, video_id, count)
+                                
+    def create_signup_favorite_user_tag_map(self, user_tag_map, user_id: str):
+        user_db = self.db['USER']
+        user_signup_favorite_list: List[str]= user_db.find_one({'_id': ObjectId(user_id)})['signup_favorite']
+        
+        for video_id in user_signup_favorite_list:
+            self.update_tag_map(user_tag_map, video_id, 1)
+    
+    def get_user_tag_map(self, user_id: str):
+        user_tag_map = {}
+        self.create_signup_favorite_user_tag_map(user_tag_map,user_id)
+        self.create_watch_history_user_tag_map(user_tag_map, user_id)
         return user_tag_map
     
-
     def get_recommendation_list(self, user_id: str, offset: int,limit=40) -> List[str]:
         user_tag_map = self.get_user_tag_map(user_id)
         video_id_list = self.find_all_video_id()
         
         candidate_music = []
-        print("video_id_list = ", len(video_id_list))
+        
         for video_id in video_id_list:
             candidate_music.append(self.get_jaccard_sim(user_tag_map, video_id))
-        
+            
         sorted_candidate_music = sorted(candidate_music, key=lambda x: -x['jaccard_sim'])
         
-        print("sroted candidate", type(sorted_candidate_music))
-    
         candidate_music_length = len(sorted_candidate_music)        
         # To avoid out of index error
         
         start_index = min(int(offset), candidate_music_length)
         limit_index = min(candidate_music_length, int(offset)+int(limit))
         
-        print("start_index = ", start_index, "limit_index = ", limit_index)
         return sorted_candidate_music[start_index:limit_index]
 
     def get_jaccard_sim(self, user_tag_map, video_id):
